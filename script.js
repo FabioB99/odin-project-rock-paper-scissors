e86ec58f1e1d08fe4f6ef71baf5f0e7186c09c36
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2026-02-27 14:53:51 +0100

    Fix main round win logic

--- script.js
+++ script.js
@@ -28,26 +28,23 @@ function getHumanChoice() {
     }
 }
 
-// Need to fix the logic here -> If I have rock and the computer has paper it should work
+
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
         console.log(`No one wins! Both chose ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}!`);
     } else if (
         humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "paper" && computerChoice === "stone" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
         humanChoice === "scissors" && computerChoice === "paper") {
         let humanChoiceCapitalized = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
         let computerChoiceCapitalized = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-        console.log(`You win! ${humanChoiceCapitalized} beats ${computerChoiceCapitalized}!`);
+        console.log(`You win! ${humanChoiceCapitalized} beats ${computerChoiceCapitalized}!`); // Fix grammer here (scissors shoudl be singular)
         humanScore++;
-    } else if (
-        computerChoice === "rock" && humanChoice === "scissors" ||
-        computerChoice === "paper" && humanChoice === "stone" ||
-        computerChoice === "scissors" && humanChoice === "paper") {
+    } else {
         let humanChoiceCapitalized = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
         let computerChoiceCapitalized = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-        console.log(`You lose! ${computerChoiceCapitalized} beats ${humanChoiceCapitalized}!`);
+        console.log(`You lose! ${computerChoiceCapitalized} beats ${humanChoiceCapitalized}!`); // Fix grammer here (scissors shoudl be singular)
         computerScore++;
     }
 }
