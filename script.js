0baa3c358afb5bcdd4388317c9da3ee95a5b17c1
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2026-02-27 12:30:56 +0100

    Write function for computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("Hello World");
+
+/*
+Pseudocode for computer choice function:
+
+CALL random int function
+Returns random integer that is either 0, 1, or 2
+STORE that value in a variable called number
+IF number is 0, return "rock"
+IF number is 1, return "paper"
+IF number is 2, return "scissor"
+
+*/
+
+function getComputerChoice() {
+    let number = getRandomInt(3);
+
+    if (number === 0) {
+        return "rock";
+    } else if (number === 1) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+console.log(getComputerChoice());
+
+
+
+
