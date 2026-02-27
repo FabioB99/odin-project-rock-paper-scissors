df5e534faf83ce85aa540ec2b19575469934efc2
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2026-02-27 12:49:14 +0100

    Add function to prompt for the human's choice

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 console.log("Hello World");
 
 /*
-Pseudocode for computer choice function:
+FUNCTION getComputerChoice
+    SET randomNumber TO result of call randomInt(0, 2)
 
-CALL random int function
-Returns random integer that is either 0, 1, or 2
-STORE that value in a variable called number
-IF number is 0, return "rock"
-IF number is 1, return "paper"
-IF number is 2, return "scissor"
-
+    IF randomNumber IS EQUAL TO 0
+        RETURN "rock"
+    ELSE IF randomNumber IS EQUAL TO 1
+        RETURN "paper"
+    ELSE IF randomNumber IS EQUAL TO 2
+        RETURN "scissors"
+    END IF
+END FUNCTION
 */
 
 function getComputerChoice() {
@@ -28,8 +30,26 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-console.log(getComputerChoice());
+console.log("Computer choice: " + getComputerChoice());
 
+/*
+FUNCTION getHumanChoice
+    CALL prompt() function and set humanChoice variable to choice 
 
+    IF humanChoice is Equal to "rock", "paper" OR scissors
+        RETURN humanChoice
+    ELSE 
+        SHOW alert to enter a valid choice
+END FUNCTION
+*/
 
+function getHumanChoice () {
+    let humanChoice = prompt("Please choose between rock, paper, or scissors");
+    if (humanChoice === "rock" | humanChoice === "paper" | humanChoice === "scissors") {
+        return humanChoice;
+    } else {
+        alert("Please enter a valid choice!");
+    }
+}
 
+console.log("Human choice: " + getHumanChoice());
